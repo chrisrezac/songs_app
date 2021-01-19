12d22b92f61bfec8d1a2397ff68f7e931b21ff44
chris_rezac <rezacchristopher@gmail.com>
2021-01-19 12:51:33 -0500

    adds three song into seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# songs = Song.create(
+#   [
+#     { 
+#       title: "The Woods", 
+#       album: "The Boy Who Cried Wolf",
+#       artist: "Olivver the Kid", 
+#       year: "2015"
+#     },
+#     {
+#       title: "Goodmorning",
+#       album: "Gone Now",
+#       artist: "Bleachers",
+#       year: "2017"
+#     },
+#     {
+#       title: "Love Always Remains",
+#       album: "Time to Pretend",
+#       artist: "MGMT",
+#       year: "2005"
+#     }
+# ]
+)
